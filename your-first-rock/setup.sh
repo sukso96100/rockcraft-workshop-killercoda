bf44b59c37b6a10ffef228951bd021763950f9f5
Youngbin Han <sukso96100@gmail.com>
2025-02-14 23:59:37 +0900

    Add verify script

--- your-first-rock/setup.sh
+++ your-first-rock/setup.sh
@@ -5,4 +5,4 @@ sudo lxd init --auto
 # https://documentation.ubuntu.com/lxd/en/latest/howto/network_bridge_firewalld/#prevent-connectivity-issues-with-lxd-and-docker
 sudo echo "net.ipv4.conf.all.forwarding=1" > /etc/sysctl.d/99-forwarding.conf
 sudo systemctl restart systemd-sysctl
-echo "===== All tools installed! Have fun! ====="
+touch /home/ubuntu/.tools-installed
